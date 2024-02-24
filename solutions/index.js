1303436afe4d80cb363117566415fec84d067324
priyanshuhaldar007 <priyanshu1843@gmail.com>
2024-02-25 01:08:06 +0530

    feat: Day1 complete

---
+++ solutions/index.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+
+function readFileContent(filePath) {
+    // Implementation
+    return new Promise((resolve, reject) => {
+        fs.readFile(filePath, "utf8", (err, data) => {
+            if (err) {
+                reject(err);
+            } else {
+				console.log(data);
+                resolve(data);
+            }
+        });
+    });
+}
+
+readFileContent('test-files/file1.txt');
+// Expected Output: Content of file1.txt
+
+readFileContent('test-files/empty-file.txt');
+// Expected Output: (empty string)
+
+readFileContent('test-files/nonexistent-file.txt');
