4d5087e6599bf114affccb8412762be6fc638ecd
priyanshuhaldar007 <priyanshu1843@gmail.com>
2024-02-25 02:50:13 +0530

    feat: Day5 completed

---
+++ solutions/day5.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+function checkFileExtension(file_path, ext) {
+    const file_ext = path.extname(file_path);
+
+    if(file_ext === ext){
+        console.log(`File has the expected extension: ${ext}`);
+    }
+    else{
+        console.log(`File does not have the expected extension. Expected: ${ext}, Actual: ${file_ext}`);
+    }
+
+}
+
+checkFileExtension('test-files/file1.txt', '.txt');
+// Expected Output: File has the expected extension: .txt
+
+checkFileExtension('test-files/image.md', '.jpg');
+// Expected Output: File does not have the expected extension. Expected: .jpg, Actual: .png
